fa1b3431ecf7f586769bb98e05a74f56c68864c9
tylerjoh <tylerjoh@hawaii.edu>
2020-10-14 12:46:46 -1000

    Lab10

--- Lab10/Arrays.js
+++ Lab10/Arrays.js
@@ -1,43 +1,44 @@
-var employees = ["Celopatra", "Gandhi", "Joan of Arc", "Abraham Lincoln", "JFK", "Jesus"];
-var my_kids = [["Kirk", 6, 59.5], ["Spock", 2, 35.2], ["McCoy", 19, 200]];
+var employees = ["James", "Phineas", "Hercules", "Batgirl", "Wonder Woman", "Jonas"];
+var my_kids = [["James", 6, 59.5], ["Goober", 2, 35.2], ["Stretch", 19, 200]];
 console.log(my_kids);
 
 var my_kid1 = {
-    "name": "Kirk",
+    "name": "James",
     "age": 6,
     "weight": 59.5
-};
+} ; 
 
 var my_kid2 = {
-    "name": "Spock",
+    "name": "Goober",
     "age": 2,
     "weight": 35.2
-};
+} ;
 
 var my_kid3 = {
-    "name": "McCoy",
+    "name": "Stretch",
     "age": 19,
     "weight": 200
-};
+} ;
 
 console.log("Age of kid3 = " + my_kid3["age"]);
-fav_kids = [my_kid1, my_kid2, my_kid3];
 
+better_kids = [my_kid1, my_kid2, my_kid3];
 
-for (i=0; i < fav_kids.length; i++) {
-    console.log("My kid " + fav_kids[i].name + ` is ${fav_kids[i].age} years old `);
+for (i=0; i<better_kids.length; i++) {
+    console.log("My kid " + better_kids[i].name + " is " + better_kids[i].age + " years old");
 }
 
-/*
-for (i=0; i < my_kids.length; i++){
-    console.log("My kid %s weighs %d pounds.", my_kids[i][0], my_kids[i][2]);
+/* for (i=0; i< my_kids.length; i++) {
+    console.log(`My kid  ${my_kids[i][0]} weighs ${my_kids[i][2]}`);
 }
-/*
-(for (i=0; i < employees.length; i++) {
-    if (i%2 == 0) {
-        console.log("Team 1 member: " + employees[i]);
-    } else {
-        console.log("Team 2 member: " + employees[i]);
+*/
+/*for (i=0; i < employees.length; i++) {
+    if (i % 2 == 0)
+    {
+        console.log("Team1 member: " + employees[i]);
+    } else
+    {
+        console.log("Team2 member: " + employees[i]);
     }
 }
 */
