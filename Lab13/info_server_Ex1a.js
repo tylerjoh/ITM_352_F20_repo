81aece262e3372443cae38c6945939eee964b80c
tylerjoh <tylerjoh@hawaii.edu>
2020-11-04 16:43:15 -1000

    Lab13

--- Lab13/info_server_Ex1a.js
+++ Lab13/info_server_Ex1a.js
@@ -1,3 +1,4 @@
+  
 var express = require('express');
 var app = express();
 
@@ -5,8 +6,4 @@ app.all('*', function (request, response, next) {
     response.send(request.method + ' to path ' + request.path);
 });
 
-app.get();
-app.post();
-
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here
-
--- Lab13/info_server_Ex2.js
+++ Lab13/info_server_Ex2.js
@@ -1,26 +1,9 @@
 var express = require('express');
 var app = express();
 var myParser = require("body-parser");
-var fs = require('fs');
-var data = require('./public/product_data.js');
-var products = data.products;
 
 app.use(express.static('./public'));
 app.use(myParser.urlencoded({ extended: true }));
-
-function process_quantity_form(POST, response) {
-    let model = products[0]['model'];
-    let model_price = products[0]['price'];
-    if (typeof POST['quantity_textbox'] != 'undefined') {
-        let q = POST['quantity_textbox'];
-        if (isNonNegInt(q)) {
-            var contents = fs.readFileSync('./views/display_quantity_template.view', 'utf8');
-            response.send(eval('`' + contents + '`')); // render template string
-        } else {
-            response.send(`${q} is not a quantity!`);
-        }
-    }
-}
 
 function isNonNegInt(stringToCheck, returnErrors = false) {
     errors = []; // assume no errors at first
@@ -37,8 +20,20 @@ app.all('*', function (request, response, next) {
 });
 
 app.post("/process_form", function (request, response) {
+    console.log("Got POST");
     let POST = request.body;
-    process_quantity_form(POST, response);
+    //response.send(POST); 
+    if (typeof POST['quantity_textbox'] != 'undefined') {
+        qty = POST["quantity_textbox"];
+        console.log(qty);
+        if (isNonNegInt(qty, false)) {
+            response.send(`<font color=blue>Thank <b>you</b> for ordering ${qty} things!</font>`);
+            //window.stop();
+        } else {
+            response.send(`${qty} is not a quantity! Press the back button and try again.`);
+        }
+    }
 });
 
 app.listen(8080, () => console.log(`listening on port 8080`));
+
