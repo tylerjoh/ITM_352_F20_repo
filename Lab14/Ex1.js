98ed8cef911f325aef90bde774137124612e5028
tylerjoh <tylerjoh@hawaii.edu>
2020-11-20 12:52:31 -1000

    Lab14

--- Lab14/Ex1.js
+++ Lab14/Ex1.js
@@ -2,9 +2,8 @@ var fs = require('fs');
 
 var filename = "user_data.json";
 
-raw_data = fs.readFileSync(filename,'utf-8');
-console.log("Success! We got: " + raw_data);
+data = fs.readFileSync(filename, 'utf-8');
+console.log("Success! We got: " + data);
 
-user_data = JSON.parse(raw_data);
-console.log(user_data);
-
+user_data = JSON.parse(data);
+console.log("User_data=", user_data);
