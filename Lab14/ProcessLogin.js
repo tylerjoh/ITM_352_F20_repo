e637946f7cd9d02a01cd71f3af1007074ca312f4
tylerjoh <tylerjoh@hawaii.edu>
2020-12-01 23:57:14 -1000

    Lab14

--- Lab14/ProcessLogin.js
+++ Lab14/ProcessLogin.js
@@ -1,25 +1,25 @@
 var express = require('express');
-var fs = require('fs');
 var app = express();
 var myParser = require("body-parser");
+var fs = require('fs');
+const { exit } = require('process');
+var queryString = require(ʻquery-stringʻ);
 
 
 app.use(myParser.urlencoded({ extended: true }));
 
 var filename = "user_data.json";
+var user_logged_in_name = "" ;  // Global variable to save user name
 
 if (fs.existsSync(filename)) {
-    fileStats = fs.statSync(filename);
-    console.log("File " + filename + " has " + fileStats.size + " characters")
+    data = fs.readFileSync(filename, 'utf-8');
+    //console.log("Success! We got: " + data);
 
-raw_data = fs.readFileSync(filename, 'utf-8');
-
-// console.log(user_data);
-
-user_data = JSON.parse(raw_data);
-console.log("User_data=", user_data);
+    user_data = JSON.parse(data);
+    console.log("User_data=", user_data);
 } else {
     console.log("Sorry can't read file " + filename);
+    exit();
 }
 
 app.get("/login", function (request, response) {
@@ -34,20 +34,28 @@ app.get("/login", function (request, response) {
 </body>
     `;
     response.send(str);
- });
+});
 
 app.post("/login", function (request, response) {
     // Process login form POST and redirect to logged in page if ok, back to login page if not
-console.log("Got a POST login request");
-POST = request.body;
-
-user_name_from_form = POST["username"];
-console.log("User name from form = " + user_name_from_form);
-if (user_data[user_name_from_form] != undefined) {
-response.send(`<H3> User ${POST["username"]} logged in`);
-} else {
-    response.send('Sorry Charlie!');
-}
+    console.log("Got a POST login request");
+    POST = request.body;
+    user_name_from_form = POST["username"];
+    password_from_from = POST["password"];
+    console.log("User name from form=" + user_name_from_form);
+    if (user_data[user_name_from_form] != undefined) {
+        // Check if password entered matches the password on file
+        if (user_data[user_name_from_form].password.equals(password_from_from))
+        {
+            // Good login
+            user_logged_in_name = user_name_from_form;
+            stringified = queryString.stringify(request.body);  // or request.query if using GET
+            response.redirect("./invoice.html?" + stringified); 
+        }
+        response.send(`<H3> User ${POST["username"]} logged in`);
+    } else {
+        response.send(`Sorry Charlie!`);
+    }
 });
 
 app.get("/register", function (request, response) {
@@ -64,13 +72,13 @@ app.get("/register", function (request, response) {
 </body>
     `;
     response.send(str);
- });
+});
 
- app.post("/register", function (request, response) {
+app.post("/register", function (request, response) {
     // process a simple register form
     POST = request.body;
-    
-    if (POST["username"] != undefined && POST['password' != undefined]) {                 // validate user input
+    console.log("Got register POST");
+    if (POST["username"] != undefined && POST['password'] != undefined) {          // Validate user input
         username = POST["username"];
         user_data[username] = {};
         user_data[username].name = username;
@@ -82,7 +90,6 @@ app.get("/register", function (request, response) {
 
         response.send("User " + username + " logged in");
     }
- });
-
+});
 
 app.listen(8080, () => console.log(`listening on port 8080`));
