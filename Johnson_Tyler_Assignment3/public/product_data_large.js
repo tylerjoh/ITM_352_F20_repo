20d1756b8db7ecf98a4b8b8cab7cd2984f9b83b9
tylerjoh <tylerjoh@hawaii.edu>
2020-12-14 11:09:20 -1000

    Oh no

---
+++ Johnson_Tyler_Assignment3/public/product_data_large.js
@@ -0,0 +1,37 @@
+// Product Data
+products_large = [
+    {
+        "model": "Metal Vent",
+        "size" : "(Large)",
+        "price": 78.00,
+        "image": "metal1.png"
+    },
+    {
+        "model": "T.H.E Short",
+        "size" : "(Large)",
+        "price": 68.00,
+        "image": "shorts1.png"
+    },
+    {
+        "model": "License to Train Jogger",
+        "size" : "(Large)",
+        "price": 128.00,
+        "image": "jogger.png"
+    },
+    {
+        "model": "Metal Vent Tech Short Sleeve 2.0",
+        "size" : "(Large)",
+        "price": 78.00,
+        "image": "metal2.png"
+    },
+    {
+        "model": "More Miles Duffel 40L",
+        "size" : "(Large)",
+        "price": 198.00,
+        "image": "bag1.png"
+    }
+];
+
+if (typeof module != 'undefined') { //if the type of the module is defined
+    module.exports.products_large = products_large; //export the product_data
+}
---
+++ Johnson_Tyler_Assignment3/public/product_data_xlarge.js
@@ -0,0 +1,37 @@
+// Product Data
+products_xlarge = [
+    {
+        "model": "Metal Vent",
+        "size" : "(X-Large)",
+        "price": 78.00,
+        "image": "metal1.png"
+    },
+    {
+        "model": "T.H.E Short",
+        "size" : "(X-Large)",
+        "price": 68.00,
+        "image": "shorts1.png"
+    },
+    {
+        "model": "License to Train Jogger",
+        "size" : "(X-Large)",
+        "price": 128.00,
+        "image": "jogger.png"
+    },
+    {
+        "model": "Metal Vent Tech Short Sleeve 2.0",
+        "size" : "(X-Large)",
+        "price": 78.00,
+        "image": "metal2.png"
+    },
+    {
+        "model": "More Miles Duffel 40L",
+        "size" : "(X-Large)",
+        "price": 198.00,
+        "image": "bag1.png"
+    }
+];
+
+if (typeof module != 'undefined') { //if the type of the module is defined
+    module.exports.products_xlarge = products_xlarge; //export the product_data
+}
