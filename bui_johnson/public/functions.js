5790776df97dc6c4859e8223383bd2a9b6192043
tylerjoh <tylerjoh@hawaii.edu>
2020-12-17 23:41:33 -1000

    rid of getCookie warning

--- bui_johnson/public/functions.js
+++ bui_johnson/public/functions.js
@@ -1,3 +1,18 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 // This function asks the server for a "service" and converts the response to text. 
 function loadJSON(service, callback) {   
     var xobj = new XMLHttpRequest();
